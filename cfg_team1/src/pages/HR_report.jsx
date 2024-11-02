3fd41c1333d46564a4152e41e397bbcfb8e5b446
ikaay1 <imgbemele@gmail.com>
2024-11-01 23:43:47 -0400

    edit boiler plate

--- cfg_team1/src/pages/HR_report.jsx
+++ cfg_team1/src/pages/HR_report.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const HR_Report = () => {
   return (
     <div>
--- cfg_team1/src/pages/SinglePaitentOverview.jsx
+++ cfg_team1/src/pages/SinglePaitentOverview.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import Sidebar from '../components/Sidebar';
 
 const SinglePaitentOverview = () => {
   return (
     <div>
-      <h1>single paintent overview</h1>
+      <Sidebar />
     </div>
   );
 };
