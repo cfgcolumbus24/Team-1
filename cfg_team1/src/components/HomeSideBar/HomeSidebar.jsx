04f62d4789b33638f9f884134bfe30c15e6b3b3f
ikaay1 <imgbemele@gmail.com>
2024-11-02 02:47:23 -0400

    make sidebar reusable

---
+++ cfg_team1/src/components/HomeSideBar/HomeSidebar.jsx
@@ -0,0 +1,18 @@
+const HomeSidebar = () => {
+  return (
+    <>
+      {/* Sidebar */}
+      <div className='sidebar'>
+        <div className='profile-icon'>
+          {' '}
+          {/* Placeholder for Profile Icon */}{' '}
+        </div>
+        <div className='sidebar-item'>Home Page</div>
+        <div className='sidebar-item'>Reports</div>
+        <div className='sidebar-item'>Log Off</div>
+      </div>
+    </>
+  );
+};
+
+export default HomeSidebar;
--- cfg_team1/src/pages/home.jsx
+++ cfg_team1/src/pages/home.jsx
@@ -1,33 +1,29 @@
-import React from 'react';
+import HomeSidebar from '../components/HomeSideBar/HomeSidebar';
 import './Home.css'; // Add your custom CSS for styling
 
 const Home = () => {
   return (
-    <div className="home-container">
-      {/* Sidebar */}
-      <div className="sidebar">
-        <div className="profile-icon"> {/* Placeholder for Profile Icon */} </div>
-        <div className="sidebar-item">Home Page</div>
-        <div className="sidebar-item">Reports</div>
-        <div className="sidebar-item">Log Off</div>
-      </div>
+    <div className='home-container'>
+      <HomeSidebar />
 
       {/* Main Chat Area */}
-      <div className="main-content">
+      <div className='main-content'>
         {/* Search Bar */}
-        <div className="search-bar">
-          <input type="text" placeholder="Search" className="search-input" />
+        <div className='search-bar'>
+          <input type='text' placeholder='Search' className='search-input' />
         </div>
 
         {/* Chat Area */}
-        <div className="chat-area">
-          <div className="chat-bubble user-query">Yap Yap Yap Blah Blah Blah </div>
-          <div className="chat-bubble response">Hello world!</div>
+        <div className='chat-area'>
+          <div className='chat-bubble user-query'>
+            Yap Yap Yap Blah Blah Blah{' '}
+          </div>
+          <div className='chat-bubble response'>Hello world!</div>
         </div>
 
         {/* Input Area */}
-        <div className="input-area">
-          <input type="text" placeholder="Type here!" className="chat-input" />
+        <div className='input-area'>
+          <input type='text' placeholder='Type here!' className='chat-input' />
         </div>
       </div>
     </div>
