b96c9aa91d12829fbe8fd6952d712e6c30a9cd2a
ikaay1 <imgbemele@gmail.com>
2024-11-02 07:30:59 -0400

    make login main page

--- cfg_team1/src/components/HomeSideBar/HomeSidebar.jsx
+++ cfg_team1/src/components/HomeSideBar/HomeSidebar.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 const HomeSidebar = () => {
@@ -6,23 +6,26 @@ const HomeSidebar = () => {
 
   return (
     <>
-      <div className="sidebar">
-        <div 
-          className="profile-icon-wrapper"
-          onClick={() => navigate('/')} 
-          style={{ cursor: 'pointer' }}
+      <div className='sidebar'>
+        <div
+          className='profile-icon-wrapper'
+          onClick={() => navigate('/chat')}
+          style={{cursor: 'pointer'}}
         >
-          <img src={logo} alt="Logo" className="profile-icon" />
+          <img src={logo} alt='Logo' className='profile-icon' />
         </div>
 
-        <div className="sidebar-items-container">
-          <div className="sidebar-item" onClick={() => navigate('/patient-overview')}>
+        <div className='sidebar-items-container'>
+          <div
+            className='sidebar-item'
+            onClick={() => navigate('/patient-overview')}
+          >
             Patient Page
           </div>
-          <div className="sidebar-item" onClick={() => navigate('/hr-report')}>
+          <div className='sidebar-item' onClick={() => navigate('/hr-report')}>
             Reports
           </div>
-          <div className="sidebar-item" onClick={() => navigate('/logout')}>
+          <div className='sidebar-item' onClick={() => navigate('/logout')}>
             Log Off
           </div>
         </div>
